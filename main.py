bef7355459ac7c3d240eb9f0407bec0837423434
Artur-Niezgoda <110778550+Artur-Niezgoda@users.noreply.github.com>
2023-02-25 16:39:27 +0100

    add files for part2

--- main.py
+++ main.py
@@ -1,21 +1,14 @@
 from flask import Flask, render_template
 import requests
 
-blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
-response = requests.get(blog_url)
-all_posts = response.json()
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html", posts=all_posts)
+    return render_template("index.html")
 
-@app.route('/post/<int:id>')
-def get_post(id):
-    for post in all_posts:
-        if post['id'] == id:
-            return render_template("post.html", single_post=post)
 
 if __name__ == "__main__":
     app.run(debug=True)
